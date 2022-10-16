ecfeebff5d012951b74a5dc2029702217bfeccf6
InColumi <max77648>
2022-10-16 20:47:47 +0300

    Added new file

---
+++ main.swift
@@ -0,0 +1,11 @@
+print("Добро пожаловать в игру \"Быки и коровы\"!")
+print("Введите длинну числа: 2 или 3 или 4. -> !")
+
+func greet(person: String) -> String {
+    let greeting = "Hello, " + person + "!"
+    return greeting
+}
+
+var name = 121231231
+
+print("(name)")
